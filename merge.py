965bd2d526a92735e3e629fc984211993fd43222
himaja4002 <25373757+himaja4002@users.noreply.github.com>
2023-10-10 11:05:40 -0400

    Update merge.py

--- merge.py
+++ merge.py
@@ -23,3 +23,47 @@ categorical_col = st.selectbox("Select a Categorical Column for Distribution Ana
 value_counts = df1[categorical_col].value_counts()
 fig_value_counts = px.bar(value_counts, title=f"Distribution of {categorical_col}")
 st.plotly_chart(fig_value_counts)
+
+import altair as alt
+
+st.header("Data Visualization with Altair")
+
+columns = df1.columns
+x_axis = st.selectbox("Select X Axis for Altair", columns)
+y_axis = st.selectbox("Select Y Axis for Altair", columns)
+
+chart = alt.Chart(df1).mark_circle().encode(
+    x=x_axis,
+    y=y_axis,
+    tooltip=[x_axis, y_axis]
+).interactive()
+
+st.altair_chart(chart, use_container_width=True)
+
+st.header("Data Quality Analysis with Altair")
+
+missing_df = pd.DataFrame({
+    'Column': df1.columns,
+    'Missing Values': df1.isnull().sum()
+})
+
+missing_chart = alt.Chart(missing_df).mark_bar().encode(
+    x='Column',
+    y='Missing Values',
+    tooltip=['Column', 'Missing Values']
+).interactive()
+
+st.altair_chart(missing_chart, use_container_width=True)
+
+
+categorical_col = st.selectbox("Select a Categorical Column for Distribution Analysis with Altair", columns)
+value_counts = df1[categorical_col].value_counts().reset_index()
+value_counts.columns = [categorical_col, 'Count']
+
+category_chart = alt.Chart(value_counts).mark_bar().encode(
+    x=categorical_col,
+    y='Count',
+    tooltip=[categorical_col, 'Count']
+).interactive()
+
+st.altair_chart(category_chart, use_container_width=True)
