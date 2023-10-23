94078cb07c59d7865ff5887b948e679b13eb219d
himaja4002 <25373757+himaja4002@users.noreply.github.com>
2023-10-23 12:07:22 -0400

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,23 +1,42 @@
-st.sidebar.title("Visualization Controls")
-st.title("Interactive Data Analysis Dashboard")
+st.markdown("""
+    <style>
+        body {
+            background-color: #1e1e1e;
+            color: #FFF;
+        }
+        .reportview-container .main .block-container {
+            padding: 2rem;
+            max-width: 1024px;
+        }
+        h2 {
+            font-weight: 600;
+            margin-bottom: 1rem;
+        }
+        .sidebar .sidebar-content {
+        background-color: #F7F7F7;
+        padding: 10px 20px;
+        }
+        select[data-baseweb="select"] {
+        min-height: 30px;
+        font-size: 14px;
+        border-radius: 8px;
+        outline: none;
+        }
+        .st-expander > div:after {
+        font-size: 16px;
+        font-weight: bold;
+        }
+        .st-expander > header {
+        padding: 8px 12px;
+        background-color: #E0E0E0;
+        }
+        .st-expander > header:hover {
+        background-color: #C0C0C0;
+        }
+    </style>
+""", unsafe_allow_html=True)
 
-# Dropdown for user to first select 'Interactive Data Visualization'
-visualization_options = ["Select an Option", "Interactive Data Visualization"]
-selected_option = st.sidebar.selectbox("Choose a Visualization Type:", visualization_options)
 
-# If the user selects 'Interactive Data Visualization' from the dropdown, display other 4 checkboxes
-if selected_option == "Interactive Data Visualization":
-    show_data_visualization = st.sidebar.checkbox("Data Visualization", value=True)
-    show_data_quality = st.sidebar.checkbox("Data Quality Analysis", value=True)
-    show_distribution_analysis = st.sidebar.checkbox("Distribution Analysis", value=True)
-    show_relational_analysis = st.sidebar.checkbox("Relational Analysis", value=True)
-else:
-    show_data_visualization = False
-    show_data_quality = False
-    show_distribution_analysis = False
-    show_relational_analysis = False
-
-columns = df1.columns
 
 # Relationship Analysis
 if show_relational_analysis:
@@ -42,3 +61,79 @@ if show_relational_analysis:
         st.warning("Please select more than one column for the scatter matrix or ensure both selected columns are categorical for categorical analysis.")
     
     # ... (rest of the code remains unchanged)
+
+st.sidebar.markdown("## Navigation", unsafe_allow_html=True)
+st.sidebar.markdown("------")
+
+col1, col2, col3 = st.sidebar.columns([1, 2, 1])  # Adjust the middle value to control width
+with col2:
+    selected_configuration_type = st.selectbox("Choose Configuration Type (Parent)", options=["-"] + CONFIGURATION_TYPES)
+
+st.sidebar.markdown("<br>", unsafe_allow_html=True) 
+
+# Main Screen Content
+if selected_configuration_type != "-":
+
+    st.write("### Configuration: ", selected_configuration_type)  # Give a title for clarity
+
+    # Load Template Section
+    with st.expander("Load Template"):
+        col1, col2, col3 = st.columns([1, 6, 1])  # Adjust the middle value to control width
+        with col2:
+            templates = st.session_state.configs.get(selected_configuration_type, {})
+            selected_template = st.selectbox("Choose Template", options=["-"] + list(templates.keys()))
+        
+        if selected_template != "-" and st.button("Load Selected Template"):
+            st.session_state.filters = templates[selected_template]
+            st.session_state.filter_added = True
+            
+            with st.spinner("Applying filters..."):
+                df = data.compute()
+                filtered_data = apply_filters(df, st.session_state.filters)
+                record_count = len(filtered_data)  
+                st.success(f"Number of records: {record_count}")
+
+    # Save New Template Section
+    with st.expander("Save New Template"):
+        new_template_name = st.text_input("Template Name")
+        
+        if new_template_name and st.button("Save Template"):
+            if selected_configuration_type not in st.session_state.configs:
+                st.session_state.configs[selected_configuration_type] = {}
+            
+            if new_template_name in st.session_state.configs[selected_configuration_type]:
+                st.warning(f"Template {new_template_name} already exists under {selected_configuration_type}. If you want to overwrite it, delete the existing one first.")
+            else:
+                st.session_state.configs[selected_configuration_type][new_template_name] = st.session_state.filters
+                save_configs_to_file(st.session_state.configs)
+                st.success(f"Saved filters as {new_template_name} under {selected_configuration_type}!")
+
+    # Delete Template Section
+    with st.expander("Delete Template"):
+        col1, col2, col3 = st.columns([1, 6, 1])  # Adjust the middle value to control width
+        with col2:
+            delete_template_name = st.selectbox("Choose Template to Delete", options=["-"] + list(templates.keys()))
+        
+        if delete_template_name != "-" and st.button("Delete Template"):
+            del st.session_state.configs[selected_configuration_type][delete_template_name]
+            save_configs_to_file(st.session_state.configs)
+            st.success(f"Deleted {delete_template_name} template under {selected_configuration_type}!")
+
+# Sidebar Content
+st.sidebar.markdown("## Navigation", unsafe_allow_html=True)
+st.sidebar.markdown("------")
+
+selected_configuration_type = st.sidebar.selectbox("Choose Configuration Type (Parent)", options=["-"] + CONFIGURATION_TYPES)
+st.sidebar.markdown("<br>", unsafe_allow_html=True)  # Adding a bit of space
+
+# Expandable Filter Section
+with st.sidebar.expander("Add New Filter", expanded=True):
+    # Include filter controls here
+
+st.sidebar.markdown("<br>", unsafe_allow_html=True)  # Adding a bit of space
+st.sidebar.markdown("## Visualization Controls", unsafe_allow_html=True)
+st.sidebar.markdown("------")
+
+
+
+
