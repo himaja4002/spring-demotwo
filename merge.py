38bb87021f7cbf0f1a3052df1da7d11172d7afa2
himaja4002 <25373757+himaja4002@users.noreply.github.com>
2023-10-23 11:25:49 -0400

    Update merge.py

--- merge.py
+++ merge.py
@@ -18,3 +18,27 @@ else:
     show_relational_analysis = False
 
 columns = df1.columns
+
+# Relationship Analysis
+if show_relational_analysis:
+    st.header("Relational Analysis")
+    default_columns = ['month', 'season']
+    sample_cols = st.sidebar.multiselect("Select columns for scatter matrix or categorical analysis", columns, default=default_columns, key='rel_select')
+    
+    # Check if selected columns are of type string
+    if all(df1[col].dtype == 'object' for col in sample_cols):
+        # If yes, perform categorical analysis
+        for col in sample_cols:
+            st.subheader(f"Category Counts for {col}")
+            category_counts = df1[col].value_counts()
+            fig_bar = px.bar(category_counts, x=category_counts.index, y=category_counts.values, title=f"Category Counts for {col}")
+            st.plotly_chart(fig_bar, use_container_width=True)
+    elif len(sample_cols) > 1:
+        fig_matrix = px.scatter_matrix(df1[sample_cols], color=df1[sample_cols[-1]], 
+                                       color_continuous_scale=px.colors.sequential.Viridis,
+                                       title="Scatter Matrix for Selected Columns")
+        st.plotly_chart(fig_matrix, use_container_width=True)
+    else:
+        st.warning("Please select more than one column for the scatter matrix or ensure both selected columns are categorical for categorical analysis.")
+    
+    # ... (rest of the code remains unchanged)
